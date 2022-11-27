14b480b2ae168d4577f366691114f528847b699b
adi201120 <119116606+adi201120@users.noreply.github.com>
2022-11-27 17:11:14 +0530

    initial commit

---
+++ src/hotel/management/system/Conn.java
@@ -0,0 +1,17 @@
+package hotel.management.system;
+
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///hotelmanagementsystem","root","pw123");  
+            s = c.createStatement();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/hotel/management/system/Pickup.java
@@ -0,0 +1,136 @@
+package hotel.management.system;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import net.proteanit.sql.*;
+
+ 
+public class Pickup extends JFrame implements ActionListener{
+    JTable table;
+    JButton back,submit;
+  Choice typeofcar;
+    JCheckBox available;
+    Pickup(){
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel text = new JLabel("Pickup Service");
+        text.setFont(new Font("Tahoma",Font.PLAIN,20));
+        text.setBounds(400,30,200,30);
+        add(text);
+        
+        JLabel lblbed = new JLabel("Type Car");
+        lblbed.setBounds(50,100,100,20);
+        add(lblbed);
+        
+        
+        typeofcar = new Choice();
+        typeofcar.setBounds(50,100,200,25);
+        add(typeofcar);
+        
+        try{
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from driver");
+            while(rs.next()){
+                typeofcar.add(rs.getString("brand"));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+            
+        
+       
+        
+        
+        
+        
+        
+       JLabel l1 = new JLabel ("Name");
+       l1.setBounds(30,160,100,20);
+       add(l1);
+       
+        JLabel l2 = new JLabel ("Age");
+       l2.setBounds(250,160,100,20);
+       add(l2);
+       
+       
+        JLabel l3 = new JLabel ("Gender");
+       l3.setBounds(410,160,100,20);
+       add(l3);
+       
+         JLabel l4 = new JLabel ("Company");
+       l4.setBounds(600,160,100,20);
+       add(l4);
+       
+         JLabel l5 = new JLabel ("brand");
+       l5.setBounds(800,160,100,20);
+       add(l5);
+     
+        
+        
+        
+        
+        table = new JTable();
+        table.setBounds(0,200,1000,300);
+        add(table);
+        
+        try{
+            Conn c = new Conn();
+            ResultSet rs  = c.s.executeQuery("select * from driver");
+            table.setModel(DbUtils.resultSetToTableModel(rs));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+         
+         submit = new JButton("Submit");
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        submit.setBounds(300,520,120,30);
+        add(submit);
+        
+        
+        
+        
+        back = new JButton("Back");
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        back.setBounds(500,520,120,30);
+        add(back);
+       
+        
+        
+         setBounds(300,200,1000,600);
+        setVisible(true);   
+        
+    }
+    
+    
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource()== submit){
+            try{
+                String query = "select * from driver where brand = '" + typeofcar.getSelectedItem()+"'";
+                
+                Conn conn = new Conn ();
+                ResultSet rs ;
+                
+                 rs = conn.s.executeQuery(query);
+                 table.setModel(DbUtils.resultSetToTableModel(rs));
+                 
+                           
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }else{
+        setVisible(false);
+        new Reception();
+        }
+    }
+    
+    public static void main(String[]args){
+        new Pickup();
+    }
+}
